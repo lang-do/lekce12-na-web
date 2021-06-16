d7b8b033a60bf88bbfdc6914471fd1ffd3eaf7e9
lang-do <do.langova@gmail.com>
2021-06-16 19:01:02 +0200

    Initial commit

---
+++ src/main/java/cz/czechitas/java2webapps/lekce12/Lekce12Application.java
@@ -0,0 +1,13 @@
+package cz.czechitas.java2webapps.lekce12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lekce12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lekce12Application.class, args);
+	}
+
+}
